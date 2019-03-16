2380ddc307caafdf7d2b6e71f78ad7d93e1221d3
Amith Liyanage <amith.liyanage.9@gmail.com>
2019-03-16 12:13:34 +0530

    create CRUD functions

--- src/app/shared/teacher.service.ts
+++ src/app/shared/teacher.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeacherService {
 
-  constructor() { }
+  constructor(private firebase: AngularFireDatabase) { }
+
+  teacherList: AngularFireList<any>;
 
   form: FormGroup= new FormGroup({
     $key: new FormControl(null),
@@ -33,4 +36,39 @@ export class TeacherService {
       isPermanent: false
     })
   };
+
+  getTeachers() {
+    this.teacherList = this.firebase.list('teachers');//if note 'teachers' node and add detail to it, else add also to it
+    return this.teacherList.snapshotChanges();
+  }
+
+  //insert dataset to firebase
+  insertTeacher(teacher) {
+    this.teacherList.push({
+      fullName: teacher.fullName,
+      email: teacher.email,
+      mobile: teacher.mobile,
+      gender: teacher.gender,
+      subject: teacher.subject,
+      hireDate: teacher.hireDate,
+      isPermanent: teacher.isPermanent
+    });
+  }
+
+  updateTeacher(teacher) {
+    this.teacherList.update(teacher.$key,
+      {
+        fullName: teacher.fullName,
+        email: teacher.email,
+        mobile: teacher.mobile,
+        gender: teacher.gender,
+        subject: teacher.subject,
+        hireDate: teacher.hireDate,
+        isPermanent: teacher.isPermanent
+      })
+  }
+
+  deleteTeacher($key: string) {
+    this.teacherList.remove($key)
+  }
 }
